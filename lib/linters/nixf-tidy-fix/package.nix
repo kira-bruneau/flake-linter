f1caeaf62dff904a77750614b88df0640a37d698
Kira Bruneau <kira.bruneau@pm.me>
2025-12-27 18:33:21 -0500

    Update nixf-tidy-fix rust edition & dependencies

--- lib/linters/nixf-tidy-fix/package.nix
+++ lib/linters/nixf-tidy-fix/package.nix
@@ -3,6 +3,6 @@
 rustPlatform.buildRustPackage {
   name = "nixf-tidy-fix";
   src = ./src;
-  cargoHash = "sha256-OCwDgWtqEsg/32cMWDxllYZhUWbRDhfdZ+N2AMsJxnU=";
+  cargoHash = "sha256-3DUAhKdlNb/c4sWlmH1K1J0m5PA21yMy+RBHiR8bALs=";
   meta.mainProgram = "nixf-tidy-fix";
 }
